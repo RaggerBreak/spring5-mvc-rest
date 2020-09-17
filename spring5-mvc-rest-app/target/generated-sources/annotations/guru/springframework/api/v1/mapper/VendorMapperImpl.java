6732a77751f9330f723ff32cd64983199e1e1959
ragger.break <ragger.break@gmail.com>
2020-09-17 13:25:47 +0200

    27.8: Testing and Running JAXB Generated Classes in Spring Boot Application

--- spring5-mvc-rest-app/target/generated-sources/annotations/guru/springframework/api/v1/mapper/VendorMapperImpl.java
+++ spring5-mvc-rest-app/target/generated-sources/annotations/guru/springframework/api/v1/mapper/VendorMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-17T11:23:37+0200",
+    date = "2020-09-17T13:20:12+0200",
     comments = "version: 1.2.0.CR2, compiler: javac, environment: Java 1.8.0_265 (Private Build)"
 )
 @Component
